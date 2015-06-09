a3bb41550e6bc776cc647ae576a2c1366f398f0e
aadusei <b.adusei@yahoo.com>
2015-06-08 21:57:41 -0500

    Added Users,general

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,15 @@
+class RegistrationController < Device::RegistrationController
+
+	private
+
+	def sign_up_params
+		params.require(:user).permit(:name, :email, :password)
+
+    end
+
+    def account_update_params
+    		params.require(:user).permit(:name, :email, :password, :current_password)
+
+    end	
+
+end
